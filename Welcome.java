967f1b2026f316a9dac435503642533f8e7fcba4
Sanchitasharda <sanchitas1607@gmail.com>
2024-03-21 22:28:09 +0530

    javaa

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,23 @@
+<<<<<<< HEAD
 public class Welcome
 {
 public static void main(String args[])
 {
 System.out.println("WELCOME TO JAVA!");
 }
+=======
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+    }
+>>>>>>> 4d709ce5bb53fb0c64447e25d1c8bf146cc8b2cb
+}public class Welcome {
+    public static void main(String[] args) {
+<<<<<<< HEAD
+        System.out.println("WELCOME TO JAVA!");
+=======
+        System.out.println("Welcome to Java!");
+>>>>>>> 4d709ce5bb53fb0c64447e25d1c8bf146cc8b2cb
+    }
 }
+
